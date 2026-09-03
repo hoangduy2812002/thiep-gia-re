1e1402d16efb7141dfb45839f665529f92fd5d6d
Duy <hoangduy2812002@gmail.com>
2026-09-03 13:39:33 +0700

    1

---
+++ listURL.js
@@ -0,0 +1,20 @@
+const listURL = [
+  {
+    id: 1,
+    ten: "Mẫu",
+    link: "https://thiep-mau-1.vercel.app/",
+    image:"/images/biaMau1.png"
+  },
+  {
+    id: 2,
+    ten: "Mẫu",
+    link: "b",
+    image:"/images/biaMau2.png"
+  },
+  // {
+  //   id: 3,
+  //   ten: "Mẫu",
+  //   link: "",
+  //   image:"/images/biaMau2.png"
+  // }
+];
--- script.js
+++ script.js
@@ -1,14 +1,54 @@
+const listProject = document.getElementById("listProject");
+
 
 document.addEventListener('DOMContentLoaded', () => {
     // openWebsite();
 });
 
 
-function openOrder (){
+function openOrder() {
     window.open("https://www.facebook.com/yud.vn", "_blank")
 }
 
-function openWebsite() {
-    window.open("https://vercel.com/", "_blank")
-
+function openWebsite(link) {
+    window.open(link, "_blank")
 }
+
+
+// ========================================
+// TẢI DANH SÁCH PROJECT
+// ========================================
+function getListProject() {
+    const data = listURL;
+    console.log(data)
+    data?.forEach((item, index) => {
+        const elementDiv = document.createElement("div");
+
+        elementDiv.className = "album-item";
+        elementDiv.id = item?.id
+        const imgDiv = document.createElement("img");
+        imgDiv.src = item?.image || '/images/noImage.png';
+        const hoverText = document.createElement("div");
+        hoverText.className = "hover-text";
+        hoverText.innerHTML = "Xem Demo";
+        const albumDiv = document.createElement("div");
+        albumDiv.className = "album-placeholder";
+        const albumNote = document.createElement("div");
+        albumNote.className = "album-note";
+        albumNote.innerHTML = "Mẫu " + (index + 1);
+
+        elementDiv.appendChild(imgDiv);
+        albumDiv.appendChild(albumNote);
+        elementDiv.appendChild(albumDiv);
+        elementDiv.appendChild(hoverText);
+
+        // Them chuc nang click vao button
+        elementDiv.addEventListener("click", (event) => {
+            openWebsite(item?.link);
+        });
+
+        listProject.appendChild(elementDiv);
+
+    });
+}
+getListProject();
